a455081f29d549b54a4e6f2d1e5e3ffa2fb240d4
hayshin <hayshinbj@gmail.com>
2025-07-19 18:58:31 +0500

    ws response data type

--- src/treaty2/ws.ts
+++ src/treaty2/ws.ts
@@ -1,91 +1,91 @@
-import type { InputSchema } from 'elysia'
-import type { Treaty } from './types'
-import { parseMessageEvent } from '../utils/parsingUtils'
+import type { InputSchema } from "elysia";
+import { parseMessageEvent } from "../utils/parsingUtils";
+import type { Treaty } from "./types";
 
 export class EdenWS<in out Schema extends InputSchema<any> = {}> {
-    ws: WebSocket
+	ws: WebSocket;
 
-    constructor(public url: string) {
-        this.ws = new WebSocket(url)
-    }
+	constructor(public url: string) {
+		this.ws = new WebSocket(url);
+	}
 
-    send(data: Schema['body'] | Schema['body'][]) {
-        if (Array.isArray(data)) {
-            data.forEach((datum) => this.send(datum))
+	send(data: Schema["body"] | Schema["body"][]) {
+		if (Array.isArray(data)) {
+			data.forEach((datum) => this.send(datum));
 
-            return this
-        }
+			return this;
+		}
 
-        this.ws.send(
-            typeof data === 'object' ? JSON.stringify(data) : data.toString()
-        )
+		this.ws.send(
+			typeof data === "object" ? JSON.stringify(data) : data.toString(),
+		);
 
-        return this
-    }
+		return this;
+	}
 
-    on<K extends keyof WebSocketEventMap>(
-        type: K,
-        listener: (event: Treaty.WSEvent<K, Schema['response']>) => void,
-        options?: boolean | AddEventListenerOptions
-    ) {
-        return this.addEventListener(type, listener, options)
-    }
+	on<K extends keyof WebSocketEventMap>(
+		type: K,
+		listener: (event: Treaty.WSEvent<K, Schema["response"][200]>) => void,
+		options?: boolean | AddEventListenerOptions,
+	) {
+		return this.addEventListener(type, listener, options);
+	}
 
-    off<K extends keyof WebSocketEventMap>(
-        type: K,
-        listener: (this: WebSocket, ev: WebSocketEventMap[K]) => any,
-        options?: boolean | EventListenerOptions
-    ) {
-        this.ws.removeEventListener(type, listener, options)
+	off<K extends keyof WebSocketEventMap>(
+		type: K,
+		listener: (this: WebSocket, ev: WebSocketEventMap[K]) => any,
+		options?: boolean | EventListenerOptions,
+	) {
+		this.ws.removeEventListener(type, listener, options);
 
-        return this
-    }
+		return this;
+	}
 
-    subscribe(
-        onMessage: (
-            event: Treaty.WSEvent<'message', Schema['response']>
-        ) => void,
-        options?: boolean | AddEventListenerOptions
-    ) {
-        return this.addEventListener('message', onMessage, options)
-    }
+	subscribe(
+		onMessage: (
+			event: Treaty.WSEvent<"message", Schema["response"][200]>,
+		) => void,
+		options?: boolean | AddEventListenerOptions,
+	) {
+		return this.addEventListener("message", onMessage, options);
+	}
 
-    addEventListener<K extends keyof WebSocketEventMap>(
-        type: K,
-        listener: (event: Treaty.WSEvent<K, Schema['response']>) => void,
-        options?: boolean | AddEventListenerOptions
-    ) {
-        this.ws.addEventListener(
-            type,
-            (ws) => {
-                if (type === 'message') {
-                    const data = parseMessageEvent(ws as MessageEvent)
+	addEventListener<K extends keyof WebSocketEventMap>(
+		type: K,
+		listener: (event: Treaty.WSEvent<K, Schema["response"][200]>) => void,
+		options?: boolean | AddEventListenerOptions,
+	) {
+		this.ws.addEventListener(
+			type,
+			(ws) => {
+				if (type === "message") {
+					const data = parseMessageEvent(ws as MessageEvent);
 
-                    listener({
-                        ...ws,
-                        data
-                    } as any)
-                } else listener(ws as any)
-            },
-            options
-        )
+					listener({
+						...ws,
+						data,
+					} as any);
+				} else listener(ws as any);
+			},
+			options,
+		);
 
-        return this
-    }
+		return this;
+	}
 
-    removeEventListener<K extends keyof WebSocketEventMap>(
-        type: K,
-        listener: (this: WebSocket, ev: WebSocketEventMap[K]) => any,
-        options?: boolean | EventListenerOptions
-    ) {
-        this.off(type, listener, options)
+	removeEventListener<K extends keyof WebSocketEventMap>(
+		type: K,
+		listener: (this: WebSocket, ev: WebSocketEventMap[K]) => any,
+		options?: boolean | EventListenerOptions,
+	) {
+		this.off(type, listener, options);
 
-        return this
-    }
+		return this;
+	}
 
-    close() {
-        this.ws.close()
+	close() {
+		this.ws.close();
 
-        return this
-    }
+		return this;
+	}
 }
